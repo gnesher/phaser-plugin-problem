c48832456dae268eacc695d609d8a19cbe29bbde
Guy Nesher <nesher.guy@gmail.com>
2019-05-16 18:24:46 +0300

    initial demo

---
+++ src/boilerplate/game.ts
@@ -0,0 +1,46 @@
+/**
+ * @author       Digitsensitive <digit.sensitivee@gmail.com>
+ * @copyright    2018 - 2019 digitsensitive
+ * @license      Digitsensitive
+ */
+
+import "phaser";
+import { MainScene } from "./scenes/mainScene";
+import VirtualJoyStickPlugin from "./rexvirtualjoystickplugin.min.js";
+
+// main game configuration
+const config: GameConfig = {
+  width: 800,
+  height: 600,
+  type: Phaser.AUTO,
+  parent: "game",
+  scene: MainScene,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 }
+    }
+  },
+  plugins: {
+    global: [{
+        key: 'rexVirtualJoyStick',
+        plugin: VirtualJoyStickPlugin,
+        start: true
+    },
+    // ...
+    ]
+}
+
+};
+
+// game class
+export class Game extends Phaser.Game {
+  constructor(config: GameConfig) {
+    super(config);
+  }
+}
+
+// when the page is loaded, create our game instance
+window.addEventListener("load", () => {
+  var game = new Game(config);
+});
